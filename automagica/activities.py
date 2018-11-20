dc3f2d073a1f97181bb2017cc81ac8701fac7181
Koen van Eijk <vaneijk.koen@gmail.com>
2018-11-20 09:07:37 +0100

    added headless and ignore images to Chrome issue #4, updated docs 🗺️

--- automagica/activities.py
+++ automagica/activities.py
@@ -472,15 +472,42 @@ Browser activities
 '''
 
 
-def ChromeBrowser():
+def ChromeBrowser(ignore_images=False, headless=False):
+    '''
+
+    Opens the Chrome Browser in a Selenium instance.
+
+    Args:
+        ignore_images (bool): do not load images
+        headless (bool): run without a window
+
+    Returns:
+        webdriver: Selenium Webdriver
+
+    Example:
+        browser = ChromeBrowser(ignore_iamges=True)
+        browser.get('https://automagica.be')
+
+    '''
     if platform.system() == 'Linux':
         chromedriver_path = '\\bin\\webdriver\\linux64\\chromedriver'
     elif platform.system() == 'Windows':
         chromedriver_path = '\\bin\\win32\\chromedriver.exe'
     else:
         chromedriver_path = '\\bin\\mac64\\chromedriver.exe'
-    from selenium.webdriver import Chrome
-    return Chrome(os.path.abspath(__file__).replace('activities.py', '') + chromedriver_path)
+
+    from selenium.webdriver import Chrome, ChromeOptions
+
+    chrome_options = ChromeOptions()
+
+    if headless:
+        chrome_options.add_argument("--headless")
+
+    if ignore_images:
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+
+    return Chrome(os.path.abspath(__file__).replace('activities.py', '') + chromedriver_path, chrome_options=chrome_options)
 
 
 def GetGoogleSearchLinks(search_text):
